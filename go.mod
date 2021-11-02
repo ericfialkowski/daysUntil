ca2c40933e813a5d2526b07696864c99641f2820
Eric Fialkowski <eric.fialkowski@oracle.com>
2021-11-02 09:17:57 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module daysUntil
+
+go 1.17
+
+require (
+	github.com/buger/goterm v1.0.3
+	github.com/hako/durafmt v0.0.0-20210608085754-5c1018a4e16b
+)
+
+require golang.org/x/sys v0.0.0-20210331175145-43e1dd70ce54 // indirect
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/buger/goterm"
+	"github.com/hako/durafmt"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Include at least one date in the form of mm-dd-yyyy as a program argument")
+		os.Exit(1)
+	}
+
+	ticker := time.NewTicker(1 * time.Second)
+
+	clearCounter := 0
+	go func() {
+		for t := range ticker.C {
+			clearCounter--
+			if clearCounter < 0 {
+				goterm.Clear()
+				clearCounter = rand.Intn(5) + 5
+			}
+			goterm.MoveCursor(1, 1)
+			_, _ = goterm.Printf("Current time: %v", t.Format(time.ANSIC))
+
+			for y, s := range os.Args[1:] {
+				goterm.MoveCursor(1, y+3)
+				date, err := time.ParseInLocation("01-02-2006", s, t.Location())
+				if err != nil {
+					_, _ = goterm.Printf("%s is not a date in the form of mm-dd-yyyy", s)
+				}
+				diff := date.Sub(t)
+				_, _ = goterm.Printf("%v until %v", durafmt.Parse(diff).LimitFirstN(3), date.Format(time.ANSIC))
+			}
+			goterm.Flush()
+		}
+	}()
+
+	select {}
+}
