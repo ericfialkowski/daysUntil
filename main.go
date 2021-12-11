729ea76c84adc40ab578f6cc80e15382bc0f656b
Eric Fialkowski <eric.fialkowski@oracle.com>
2021-12-10 17:29:16 -0700

    don't show past dates

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 	ticker := time.NewTicker(1 * time.Second)
 
 	clearCounter := 0
+	linesPrinted := 0
 	go func() {
 		for t := range ticker.C {
 			clearCounter--
@@ -32,19 +33,31 @@ func main() {
 			goterm.MoveCursor(1, 1)
 			_, _ = goterm.Printf("Current time: %v", t.Format(time.ANSIC))
 
-			for y, s := range os.Args[1:] {
-				goterm.MoveCursor(1, y+3)
+			y := 2
+			for _, s := range os.Args[1:] {
 				format := DATEFORMAT
 				if len(s) == len(DATETIMEFORMAT) {
 					format = DATETIMEFORMAT
 				}
 				date, err := time.ParseInLocation(format, s, t.Location())
-				if err != nil {
-					_, _ = goterm.Printf("%s is not a date in the form of mm-dd-yyyy or mm-dd-yyyy hh:mm", s)
+				if err != nil || date.After(time.Now()) {
+					y = y + 1
+					goterm.MoveCursor(1, y)
+					if err != nil {
+						_, _ = goterm.Printf("%s is not a date in the form of mm-dd-yyyy or mm-dd-yyyy hh:mm", s)
+					}
+					diff := date.Sub(t)
+					_, _ = goterm.Printf("%30v until %v                  ", durafmt.Parse(diff).LimitFirstN(3), date.Format(time.ANSIC))
 				}
-				diff := date.Sub(t)
-				_, _ = goterm.Printf("%30v until %v         ", durafmt.Parse(diff).LimitFirstN(3), date.Format(time.ANSIC))
 			}
+
+			if linesPrinted > y {
+				for z := y + 1; z < linesPrinted; z++ {
+					goterm.MoveCursor(1, z)
+					_, _ = goterm.Print("                                                             ")
+				}
+			}
+			linesPrinted = y
 			_, _ = goterm.Println()
 			goterm.Flush()
 		}
